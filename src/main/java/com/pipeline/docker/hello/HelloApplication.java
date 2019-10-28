dfb74d5d4127fca9c4a0f8b041299c93b4418b5b
Jack Chung <jackchung@jfrogchina.com>
2019-10-28 23:13:54 +0800

    Initial Commit

---
+++ src/main/java/com/pipeline/docker/hello/HelloApplication.java
@@ -0,0 +1,28 @@
+package com.pipeline.docker.hello;
+
+import java.util.Date;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+	}
+}
+
+@RestController
+class HelloRestController {
+	@RequestMapping("/hello/{name}")
+	public String helloDocker(@PathVariable(value = "name") String name) {
+		String response = "Hello " + name + " Response received on : " + new Date();
+		System.out.println(response);
+		return response;
+
+	}
+}
